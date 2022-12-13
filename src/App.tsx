78c8de5cffc3c115492b4ecf484ac994858105e1
nns7 <nns7snn@gmail.com>
2022-12-13 21:42:22 +0900

    :+1: getSate()でのstoreへのアクセス。

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import store from "./store/Index";
 
 function App() {
   return (
     <div>
       <h1>Redux Learn</h1>
+      <p>Count: {store.getState().count}</p>
     </div>
   );
 }
